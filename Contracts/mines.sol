4af76cbddc304ba5f3b8c4545de148ad32a09a9b
Siddhesh Potdar <63591546+Siddhesh-Potdar@users.noreply.github.com>
2022-07-14 13:08:21 +0530

    Update mines.sol

--- Contracts/mines.sol
+++ Contracts/mines.sol
@@ -26,8 +26,6 @@ contract mines {
 
     mapping ( uint => mines_detail ) public minesdetail;
     mapping (uint => mapping (uint => ore_details) ) public oredetails; 
-    
-   
 
     function batch(uint mine_id,string memory _amount, string memory ore_type, string memory _grade, string memory _file) public{
         oredetails[mine_id][batch_id]=ore_details(_amount,ore_type,_grade,_file);
