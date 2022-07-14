02fe0f4ed8aec94ad56ce9bce0f71e7b5a9a0980
Siddhesh Potdar <63591546+Siddhesh-Potdar@users.noreply.github.com>
2022-07-14 13:09:33 +0530

    Update goverment_official.sol
    added mine registration function

--- Contracts/goverment_official.sol
+++ Contracts/goverment_official.sol
@@ -4,8 +4,11 @@ import "./mines.sol";
 
 contract goverment_official is mines{
     
-     function approve_batch() public{
-
+     function approve_batch() public{  }
+     
+     function mine_detail(string memory _mine_name, string memory _location, string memory _owner_name, string memory _email, string memory _phone_no, string memory _block_no, string memory _gst_no, string memory _lease_periog) public{
+        minesdetail[id] = mines_detail(_mine_name,_location,_owner_name,_email,_phone_no,_block_no,_gst_no,_lease_periog,true);
+        id=id+1;
     }
     
 }
