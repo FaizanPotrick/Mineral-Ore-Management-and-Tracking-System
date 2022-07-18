a201f2372510606d373529ac36a1e278caa37ec5
Preet552001 <102042528+Preet552001@users.noreply.github.com>
2022-07-18 06:58:35 -0700

    small update in goverment_official.sol

--- Contracts/goverment_official.sol
+++ Contracts/goverment_official.sol
@@ -1,15 +1,15 @@
-pragma solidity >=0.5.0 <0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 import "./mines.sol";
 
 
 contract goverment_official is mines{
-       uint id=1;
-    
-     function approve_batch() public{  }
-     
-     function mine_detail(string memory _mine_name, string memory _location, string memory _owner_name, string memory _email, string memory _phone_no, string memory _block_no, string memory _gst_no, string memory _lease_periog) public{
+ 
+    function mine_detail(string memory _mine_name, string memory _location, string memory _owner_name, string memory _email, string memory _phone_no, string memory _block_no, string memory _gst_no, string memory _lease_periog) public{
         minesdetail[id] = mines_detail(_mine_name,_location,_owner_name,_email,_phone_no,_block_no,_gst_no,_lease_periog,true);
+        batch_number[id]=1;
         id=id+1;
+        
     }
     
 }
