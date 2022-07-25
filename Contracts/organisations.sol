9470e58e789299d445be6fbbdc19292fad04c62d
Preet552001 <102042528+Preet552001@users.noreply.github.com>
2022-07-25 12:56:01 +0530

    Update organisations.sol

--- Contracts/organisations.sol
+++ Contracts/organisations.sol
@@ -16,7 +16,7 @@ contract organisations is mines{
     function selling(string calldata transaction_id,string calldata mine_id,string calldata buyer_organisation_id,uint amount,string calldata grade,string calldata price,string calldata ewaybill)external {
         require(keccak256(abi.encodePacked((mine[mine_id].mine_id))) == keccak256(abi.encodePacked((mine_id))),"Provided Seller Mine ID Doesn't Exist");
         require(keccak256(abi.encodePacked((organisation[buyer_organisation_id].organisation_id))) == keccak256(abi.encodePacked((buyer_organisation_id))),"Provided Seller Organisation ID Doesn't Exist");        
-        
+        require(mineBatchAmount[mine_id][grade]>= amount,"Amount of Ore is Not Present in the Mine");
         string storage mine_organisation_id=mine[mine_id].organisation_id;
         transaction[transaction_id]=transaction_details(transaction_id,mine_id,mine_organisation_id,buyer_organisation_id,amount,grade,ewaybill,price);
         mineBatchAmount[mine_id][grade] -= amount;
