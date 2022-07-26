47659de9c8343191ab82ecf89e6a3df821eb9f2b
Siddhesh Potdar <63591546+Siddhesh-Potdar@users.noreply.github.com>
2022-07-26 14:48:05 +0530

    Update user.sol

--- Contracts/user.sol
+++ Contracts/user.sol
@@ -4,9 +4,13 @@ pragma solidity >=0.4.22 <0.9.0;
 contract user{
       
     mapping (string => string) public user_detail;
+    mapping (string => string) public region_detail;
 
-      function userdetails (string calldata user_id ,string calldata userDetail_hash) external {
+      function userDetails(string calldata user_id ,string calldata userDetail_hash) external {
           user_detail[user_id]=userDetail_hash;
+      }
+       function regionDetails(string calldata region_id ,string calldata regionDetail_hash) external {
+          region_detail[region_id]=regionDetail_hash;
       }
 
 }
