03b47fb249aac627444f9fda15eb78b25039ea9f
Siddhesh Potdar <63591546+Siddhesh-Potdar@users.noreply.github.com>
2022-07-27 13:57:53 +0530

    Update mines.sol

--- Contracts/mines.sol
+++ Contracts/mines.sol
@@ -42,6 +42,6 @@ contract mines{
     mapping (string => mapping (string => ore_details) ) public batch;
     mapping( string => mapping (string => bool)) public batchState;
     mapping (string => mapping (string => mapping(string=> uint))) public mineOreAmount;
-    mapping (string=> transaction_details) transactionPartner;
+    mapping (string=> transaction_details) transaction;
 
 }
