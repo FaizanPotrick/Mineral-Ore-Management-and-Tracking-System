6e55fa7ed66ee6cf0919b2998d1b5deda6fdaea2
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2022-07-30 22:37:35 +0530

    display the used server port on console

--- server/app.js
+++ server/app.js
@@ -34,4 +34,6 @@ app.use(require("./routes/registration/Organization.js"));
 app.use(require("./routes/registration/Region.js"));
 app.use(require("./routes/Login.js"));
 app.use(require("./routes/miner/ores"));
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8000, ()=>{
+  console.log("server is running on port 8000");
+});
