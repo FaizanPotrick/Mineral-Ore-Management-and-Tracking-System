e94438effa801e69d10291d33e80174b00243d94
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2022-08-03 13:44:03 +0530

    feat: create Organisation class

---
+++ server/blockchain_scripts/organisation.js
@@ -0,0 +1,17 @@
+import './connection.js';
+
+
+class Organisation extends BlockchainConnection {
+
+    constructor() {
+        connectToContract();
+    }
+
+
+
+}
+
+
+module.exports = {
+    Organisation
+  };
