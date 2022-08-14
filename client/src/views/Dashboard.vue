d3ce24fd00387e431ad0bc1b4f1d12e2bc64b3c9
darshikapongallu <darshikapongallu456@gmail.com>
2022-08-14 12:06:37 +0530

    Updates in profile section

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -33,15 +33,154 @@ useHomeStore().user_fetch();
           {{ button.name }}
         </RouterLink>
       </div>
-      <button
+      <!-- <button 
         @click="onToggle"
         class="text-black bg-yellow-100/60 hover:bg-yellow-300 rounded-xl py-2.5 px-4 capitalize shadow-md"
       >
         Profile
+      </button> -->
+
+      <!-- VUE JS POP OVER  -->
+      <button
+        @click="showPopover()"
+        class="text-black bg-yellow-100/60 hover:bg-yellow-300 rounded-xl py-2.5 px-4 capitalize shadow-md"
+      >
+        Profile
       </button>
+      <!-- END OF VUE JSPOP OVER -->
 
-       
+      <!-- TAILWIND CSS POP OVER -->
+      <!-- <button 
+        data-popover-target="popover-user-profile"
+        class="text-black bg-yellow-100/60 hover:bg-yellow-300 rounded-xl py-2.5 px-4 capitalize shadow-md"
+      >
+        Profile
+      </button> -->
+      <!-- END OF TAILWIND CSS POP OVER -->
 
+      <!-- TAILWIND CSS POP OVER -->
+      <!-- <div id="popover-user-profile" role="tooltip" class="inline-block absolute invisible z-10 w-64 text-sm font-light text-gray-500 bg-white rounded-lg border border-gray-200 shadow-sm opacity-0 transition-opacity duration-300 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
+    <div class="p-3">
+        <div class="flex justify-between items-center mb-2">
+            <a href="#">
+                <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese Leos">
+            </a>
+            <div>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Follow</button>
+            </div>
+        </div>
+        <p class="text-base font-semibold leading-none text-gray-900 dark:text-white">
+            <a href="#">Jese Leos</a>
+        </p>
+        <p class="mb-3 text-sm font-normal">
+            <a href="#" class="hover:underline">@jeseleos</a>
+        </p>
+        <p class="mb-4 text-sm font-light">Open-source contributor. Building <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">flowbite.com</a>.</p>
+        <ul class="flex text-sm font-light">
+            <li class="mr-2">
+                <a href="#" class="hover:underline">
+                    <span class="font-semibold text-gray-900 dark:text-white">799</span>
+                    <spa>Following</spa>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline">
+                    <span class="font-semibold text-gray-900 dark:text-white">3,758</span>
+                    <span>Followers</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div data-popper-arrow></div>
+</div> -->
+      <!-- END TAILWIND CSS POP OVER -->
+
+      <!-- VUE JS Code block starts -->
+      <div
+        id="popover"
+        class="transition duration-150 ease-in-out mt-16 absolute w-full sm:w-1/2"
+      >
+        <div class="w-full bg-white rounded shadow-2xl">
+          <div class="w-full h-full px-4 xl:px-8 pt-3 pb-5">
+            <div class="flex justify-between items-center">
+              <div class="flex items-center">
+                <div>
+                  <h3
+                    class="mb-2 sm:mb-1 text-gray-800 text-base font-normal leading-4"
+                  >
+                    Aadhar Card
+                  </h3>
+                  <p class="text-gray-600 text-xs leading-3">ABC</p>
+                </div>
+              </div>
+            </div>
+            <div class="py-6 flex justify-between items-center">
+              <div class="flex items-center">
+                <div>
+                  <h3
+                    class="mb-2 sm:mb-1 text-gray-800 text-base font-normal leading-4"
+                  >
+                    Registration Number
+                  </h3>
+                  <p class="text-gray-600 text-xs leading-3">XYZ</p>
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between items-center">
+              <div class="flex items-center">
+                <div>
+                  <h3
+                    class="mb-2 sm:mb-1 text-gray-800 text-base font-normal leading-4"
+                  >
+                    Registerd Mine
+                  </h3>
+                  <p class="text-gray-600 text-xs leading-3">PQR</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- VUE JS Code block ends -->
+
+      <!-- TAILWIND POP OVER  -->
+
+      <!-- <div id="popover-user-profile" role="tooltip" class="inline-block absolute invisible z-10 w-64 text-sm font-light text-gray-500 bg-white rounded-lg border border-gray-200 shadow-sm opacity-0 transition-opacity duration-300 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top" style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(278.4px, 218.4px, 0px);">
+        <div class="p-3">
+            <div class="flex justify-between items-center mb-2">
+                <a href="#">
+                    <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese Leos">
+                </a>
+                <div>
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Follow</button>
+                </div>
+            </div>
+            <p class="text-base font-semibold leading-none text-gray-900 dark:text-white">
+                <a href="#">Jese Leos</a>
+            </p>
+            <p class="mb-3 text-sm font-normal">
+                <a href="#" class="hover:underline">@jeseleos</a>
+            </p>
+            <p class="mb-4 text-sm font-light">Open-source contributor. Building <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">flowbite.com</a>.</p>
+            <ul class="flex text-sm font-light">
+                <li class="mr-2">
+                    <a href="#" class="hover:underline">
+                        <span class="font-semibold text-gray-900 dark:text-white">799</span>
+                        <spa>Following
+                    </spa></a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">
+                        <span class="font-semibold text-gray-900 dark:text-white">3,758</span>
+                        <span>Followers</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div data-popper-arrow="" style="position: absolute; left: 0px; transform: translate3d(123.2px, 0px, 0px);"></div>
+    </div> -->
+
+      <!-- END TAILWIND POP OVER -->
       <button
         @click="useLoginStore().logout_fn(router)"
         class="text-yellow-700 hover:text-gray-900"
@@ -49,9 +188,7 @@ useHomeStore().user_fetch();
         Logout
       </button>
     </div>
-
-
-    <transition name="fade">
+    <!-- <transition name="fade">
       <div v-if="isModalVisible">
         <div
           @click="onToggle"
@@ -83,8 +220,7 @@ useHomeStore().user_fetch();
           </div>
         </div>
       </div>
-    </transition>
-
+    </transition> -->
 
     <RouterView class="p-10 w-full min-h-[92vh]" />
   </div>
@@ -101,7 +237,7 @@ useHomeStore().user_fetch();
   </div>
 </template>
 
-<script>
+<!-- <script>
 export default {
   data() {
     return {
@@ -141,4 +277,37 @@ export default {
 .fade-leave-to {
   opacity: 0;
 }
-</style>
+</style> -->
+
+<!-- vue js popover -->
+<script>
+export default {
+  name: "SimplePopover",
+  methods: {
+    showPopover() {
+      document.getElementById("popover").classList.toggle("opacity-0");
+    },
+    dropdownFunction(flag) {
+      let upIcon;
+      let downIcon;
+      if (flag === 1) {
+        upIcon = document.getElementById("upIcon1");
+        downIcon = document.getElementById("downIcon1");
+        upIcon.classList.toggle("hidden");
+        downIcon.classList.toggle("hidden");
+      } else if (flag === 2) {
+        upIcon = document.getElementById("upIcon2");
+        downIcon = document.getElementById("downIcon2");
+        upIcon.classList.toggle("hidden");
+        downIcon.classList.toggle("hidden");
+      } else if (flag === 3) {
+        upIcon = document.getElementById("upIcon3");
+        downIcon = document.getElementById("downIcon3");
+        upIcon.classList.toggle("hidden");
+        downIcon.classList.toggle("hidden");
+      }
+    },
+  },
+};
+</script>
+<!-- end of vue js popover -->
