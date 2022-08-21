0766954183b02a07480e705a70323cde69249040
Preet552001 <102042528+Preet552001@users.noreply.github.com>
2022-08-20 21:59:28 -0700

    adding verify function

--- blockchain/contracts/mining.sol
+++ blockchain/contracts/mining.sol
@@ -26,4 +26,22 @@ contract mining{
     function createTransaction (string calldata transaction_id,string calldata _mine_id,string calldata _org_id,string calldata _transaction_hash,string calldata _doc_hash) external{
         transaction[transaction_id]=transaction_details(_mine_id,_org_id,_transaction_hash,_doc_hash);
     }
+
+    function verifyMineBatch (string calldata batch_id, string calldata _batch_hash, string calldata _doc_hash) external view returns(bool) {
+        if (keccak256(abi.encodePacked(batch[batch_id].batch_hash)) == keccak256(abi.encodePacked(_batch_hash)) && keccak256(abi.encodePacked(batch[batch_id].doc_hash)) == keccak256(abi.encodePacked(_doc_hash))){
+            return(true);
+        }
+        else{
+            return(false);
+        }
+    }
+
+    function verifyTransaction (string calldata transaction_id, string calldata _transaction_hash, string calldata _doc_hash) external view returns(bool) {
+        if (keccak256(abi.encodePacked(transaction[transaction_id].transaction_hash)) == keccak256(abi.encodePacked(_transaction_hash)) && keccak256(abi.encodePacked(transaction[transaction_id].doc_hash)) == keccak256(abi.encodePacked(_doc_hash))){
+            return(true);
+        }
+        else{
+            return(false);
+        }
+    }    
 }
