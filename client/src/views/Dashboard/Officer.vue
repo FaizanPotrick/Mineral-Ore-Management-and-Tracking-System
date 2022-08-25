ec23a9e8cc2db55c745f109dd45be632472ee974
ruchi-115 <ruchitasonawale115@gmail.com>
2022-08-25 10:07:32 +0530

    cards changes

--- client/src/views/Dashboard/Officer.vue
+++ client/src/views/Dashboard/Officer.vue
@@ -10,11 +10,10 @@ const markers = ref([]);
 
 const dashboard = async () => {
   const { data } = await axios.get(
-    `/api/dashboard/officer/${
-      route.params.region_type === undefined &&
+    `/api/dashboard/officer/${route.params.region_type === undefined &&
       route.params.region_id === undefined
-        ? $cookies.get("type_of_region")
-        : `${route.params.region_type}?region_id=${route.params.region_id}`
+      ? $cookies.get("type_of_region")
+      : `${route.params.region_type}?region_id=${route.params.region_id}`
     }`
   );
   title.value = data.title;
@@ -48,24 +47,18 @@ onBeforeMount(() => {
       </div>
     </div>
     <div class="flex gap-4 flex-wrap font-semibold">
-      <div
-        :key="card"
-        v-for="card of cards"
-        class="flex flex-col border-l-4 shadow-2xl border-yellow-300 py-2 px-2 bg-white rounded-3xl rounded-br-3xl border-r-3 drop-shadow-md min-w-[17rem]"
-      >
+      <div :key="card" v-for="card of cards"
+        class="flex flex-col border-l-4 shadow-2xl border-yellow-300 py-2 px-2 bg-white rounded-3xl rounded-br-3xl border-r-3 drop-shadow-md min-w-[17rem]">
         <div class="text-xl text-center border-b-4 border-yellow-300">
           {{ card.title }}
         </div>
 
         <div class="text-center text-5xl mb-1 my-2">
           <!-- <hr class="border-yellow-300 border-1"> -->
-          <div
-            v-if="typeof card.value === 'object'"
-            v-for="(value, name) of card.value"
-          >
+          <div v-if="typeof card.value === 'object'" v-for="(value, name) of card.value">
             {{ name }} : {{ value }}
           </div>
-          <div v-else>{{ card.value }}</div>
+          <div v-else>{{ Math.round(card.value) }}</div>
         </div>
       </div>
     </div>
@@ -75,16 +68,8 @@ onBeforeMount(() => {
         <ol-source-osm />
       </ol-tile-layer>
       <ol-zoom-control />
-      <ol-overlay
-        :key="marker._id"
-        v-for="marker of markers"
-        :position="marker.coordinates"
-      >
-        <img
-          src="@/assets/marker.png"
-          class="h-8 w-8 cursor-pointer"
-          alt="marker"
-        />
+      <ol-overlay :key="marker._id" v-for="marker of markers" :position="marker.coordinates">
+        <img src="@/assets/marker.png" class="h-8 w-8 cursor-pointer" alt="marker" />
       </ol-overlay>
     </ol-map>
   </div>
