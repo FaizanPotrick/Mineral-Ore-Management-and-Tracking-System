332f296d17bb06297cd91762860e5e5a455a84e7
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2022-08-26 04:16:08 +0530

    feat: warehouse register

--- server/models/WarehouseSchema.js
+++ server/models/WarehouseSchema.js
@@ -62,20 +62,14 @@ const Warehouse = new mongoose.Schema({
       },
     },
   },
-  location: {
-    pin_code: {
+  coordinates: {
+    latitude: {
       type: Number,
       required: true,
     },
-    coordinates: {
-      latitude: {
-        type: Number,
-        required: true,
-      },
-      longitude: {
-        type: Number,
-        required: true,
-      },
+    longitude: {
+      type: Number,
+      required: true,
     },
   },
 });
